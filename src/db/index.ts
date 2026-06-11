be64672ea14b5f28d482b972443528c25246a5d2
Eliaz Bobadilla <eliaz@bobadilla.tech>
2026-06-11 13:35:22 -0500

    fix: export db client

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,4 +2,4 @@ import { drizzle } from "drizzle-orm/libsql";
 import { env } from "../lib/env";
 import * as schema from "./schema";
 
-const db = drizzle(env.DB_FILE_NAME, { schema });
+export const db = drizzle(env.DB_FILE_NAME, { schema });
